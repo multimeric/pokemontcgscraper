d7dc49a11fba56fae2ef9e528602cae3c5c7eeac
Michael Milton <ttmigueltt@gmail.com>
2016-08-21 12:45:49 +1000

    Convert tests to co; check URL return status not content

--- test.js
+++ test.js
@@ -1,125 +1,115 @@
 #!/usr/bin/env node --harmony
-var scraper = require("./index");
-var assert = require("assert");
-var Promise = require('bluebird');
+const scraper = require("./index");
+const assert = require("assert");
+const request = require('request-promise');
+const co = require('co');
+require('co-mocha');
 
-describe("scrapeSearchPage", function () {
+function checkResponse(response, content_type){
+    assert(response.statusCode == 200);
+    assert(response.headers['content-type'].includes(content_type));
+}
+
+describe("scrapeSearchPage", () => {
 
     //Scrape the first page of the unfiltered results
-    it("Scrapes an array of cards and a page count", function (done) {
-        scraper.scrapeSearchPage("http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/?None").then(function (page) {
-            assert(page.numPages > 400);
-            assert(page.cards.length >= 12);
-            done();
-        }).catch(function(err){
-        	done(err);
-        });
+    it("Scrapes an array of cards and a page count", function *() {
+        const page = yield scraper.scrapeSearchPage("http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/?None");
+        assert(page.numPages > 400);
+        assert(page.cards.length >= 12);
     });
 
-    //Scrape a search page with just a Snivy on it
-    it("Returns name, image, and id", function (done) {
-        scraper.scrapeSearchPage("http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/?cardName=snivy&cardText=slam").then(function (page) {
-            var card = page.cards[0];
-            assert(card.url == "http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/bw-series/bwp/BW01/");
-            assert(card.image == "http://assets21.pokemon.com/assets/cms2/img/cards/web/BWP/BWP_EN_BW01.png");
-            assert(card.id == "bwp/BW01");
-            done();
-        }).catch(function(err){
-        	done(err);
+//Scrape a search page with just a Snivy on it
+    it("Returns name, image, and id", function *() {
+        const page = yield scraper.scrapeSearchPage("http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/?cardName=snivy&cardText=slam");
+        const card = page.cards[0];
+
+        //Check the URL
+        const urlResp = yield request({
+            resolveWithFullResponse: true,
+            url: card.url
         });
+        checkResponse(urlResp, 'html');
+
+        //Check the image
+        const imgResp = yield request({
+            resolveWithFullResponse: true,
+            url: card.image
+        });
+        checkResponse(imgResp, 'image');
     });
 });
 
-describe("scrapeCard", function () {
+describe("scrapeCard", () => {
 
-    it("Correctly scrapes trainers", function (done) {
+    it("Correctly scrapes trainers", function *() {
         //Scrape N
-        scraper.scrapeCard("http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/bw-series/bwp/BW100/").then(function (card) {
-            assert(card.name == "N");
-            assert(card.text.indexOf("Prize cards" != -1));
-            done();
-        }).catch(function(err){
-        	done(err);
-        });
-
+        const card = yield scraper.scrapeCard("http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/bw-series/bwp/BW100/");
+        assert(card.name == "N");
+        assert(card.text.includes("Prize cards"));
     });
 
-    it("Correctly scrapes pokemon", function (done) {
+    it("Correctly scrapes pokemon", function *() {
         //Scrape Snivy
-        scraper.scrapeCard("http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/bw-series/bwp/BW01/").then(function (card) {
-            assert(card.name == "Snivy");
-            assert(card.hp == 60);
-            assert(card.abilities[0].name == "Slam");
-            assert(card.image == "http://assets21.pokemon.com/assets/cms2/img/cards/web/BWP/BWP_EN_BW01.png");
-            assert(card.id == "bwp/BW01");
-            done();
-        }).catch(function(err){
-        	done(err);
+        const card = yield scraper.scrapeCard("http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/bw-series/bwp/BW01/");
+        assert(card.name == "Snivy");
+        assert(card.hp == 60);
+        assert(card.abilities[0].name == "Slam");
+        //Check the image
+        const imgResp = yield request({
+            resolveWithFullResponse: true,
+            url: card.image
         });
+        checkResponse(imgResp, 'image');
+        assert(card.id == "bwp/BW01");
     });
 
-    it("Handles mega EXs", function (done) {
-        scraper.scrapeCard("http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/xy-series/xy1/2/").then(function (card) {
-            assert(card.name == "M Venusaur-EX");
-            assert(card.hp == 230);
-            assert(card.abilities[0].name == "Crisis Vine");
-            done();
-        }).catch(function(err){
-        	done(err);
-        });
+    it("Handles mega EXs", function *() {
+        const card = yield scraper.scrapeCard("http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/xy-series/xy1/2/");
+        assert(card.name == "M-Venusaur-EX");
+        assert(card.hp == 230);
+        assert(card.abilities[0].name == "Crisis Vine");
     });
 
-    it("Correctly scrapes different types of pokemon abilities", function (done) {
-        var toScrape = [
+    it("Correctly scrapes different types of pokemon abilities", function *() {
+        const toScrape = [
             'http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/bw-series/bw7/31/',
             'http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/diamond-pearl-series/dp3/2/',
             'http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/ex-series/ex14/14/',
             'http://www.pokemon.com/us/pokemon-tcg/pokemon-cards/ex-series/ex6/104/'].map(function (url) {
-                return scraper.scrapeCard(url);
-            });
-
-        Promise.all(toScrape).map(function (card) {
-            assert("passive" in card);
-        }).then(function () {
-            done();
-        }).catch(function(err){
-        	done(err);
+            return scraper.scrapeCard(url);
         });
+
+        for (const card of yield toScrape) {
+            assert("passive" in card);
+        }
     });
 });
 
 describe("scrapeAll", function () {
 
-    it("scrapes multi page queries", function (done) {
-    	this.timeout(0);
+    it("scrapes multi page queries", function *() {
+        this.timeout(0);
 
-        scraper.scrapeAll({
+        const cards = yield scraper.scrapeAll({
             cardName: "saur"
-        }, false).then(function (cards) {
-            assert(Array.isArray(cards));
-            assert(cards.length >= 24);
-            done();
-        }).catch(function(err){
-        	done(err);
-        });
+        }, false);
+        assert(Array.isArray(cards));
+        assert(cards.length >= 24);
     });
 
-    it("scrapes card details when scrapeDetails is true", function (done) {
-    	this.timeout(0);
+    it("scrapes card details when scrapeDetails is true", function *() {
+        this.timeout(0);
 
-        scraper.scrapeAll({
+        const cards = scraper.scrapeAll({
             cardName: "saur"
-        }, true).then(function (cards) {
+        }, true);
 
-        	//At least one of the '*saur' pokemon should have Vine Whip
-            assert(cards.some(function(saur){
-        		return saur.abilities.some(function(attack){
-        			return attack.name == "Vine Whip";
-        		});
-            }));
-            done();
-        }).catch(function(err){
-        	done(err);
-        });
+        //At least one of the '*saur' pokemon should have Vine Whip
+        assert(cards.some(function (saur) {
+            return saur.abilities.some(function (attack) {
+                return attack.name == "Vine Whip";
+            });
+        }));
     });
 });
